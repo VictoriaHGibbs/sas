4437b4dbff76b4e09918260d7c0ee9e3533d6b86
Victoria Gibbs <victoriahgibbs@gmail.com>
2024-02-26 22:12:56 -0500

    Code for phase01 is ready

--- phase01/public/salamanders/index.php
+++ phase01/public/salamanders/index.php
@@ -1,4 +1,4 @@
-<!-- require initialize.php -->
+<?php require_once('../../private/initialize.php'); ?>
 
 
 <!-- 
@@ -8,13 +8,17 @@ id=2, salamanderName = Pigeon Mountain Salamander
 id=3', salamanderName = ZigZag Salamander
 id=4,  salamanderName= Slimy Salamander 
 -->
-
-
-
-<!-- Add the pageTitle for salamanders
-Include a shared path to the salamander header -->
-
+<?php
+  $salamanders = [
+    ['id' => '1', 'salamanderName' => 'Red-Legged Salamander'],
+    ['id' => '2', 'salamanderName' => 'Pigeon Mountain Salamander'],
+    ['id' => '3', 'salamanderName' => 'ZigZag Salamander'],
+    ['id' => '4', 'salamanderName' => 'Slimy Salamander'],
+  ]
 ?>
+
+<?php $page_title = 'SAS - Salamanders'; ?>
+<?php include(SHARED_PATH . '/salamander-header.php'); ?>
 
 <h1>Salamanders</h1>
 
@@ -31,13 +35,13 @@ Include a shared path to the salamander header -->
 
       <?php foreach($salamanders as $salamander) { ?>
         <tr>
-          <!-- <td>Display the salamander id</td> -->
-    	    <!-- <td>Display the salamander name</td> -->
-          <!-- Use url_for with show.php AND h(u) with the salamander['id'] -->
+          <td><?php echo h($salamander['id']); ?></td>
+    	    <td><?php echo h($salamander['salamanderName']); ?></td>
+          <td><a href="<?php echo urlFor('/salamanders/show.php?id=' . h(u($salamander['id']))); ?>">View</a></td>
           <td><a href="#">Edit</a></td>
           <td><a href="#">Delete</a></td>
     	  </tr>
       <?php } ?>
   	</table>
 
-<!-- <?php add the shared path to the salamander footer ?> -->
+<?php include(SHARED_PATH . '/salamander-footer.php') ?>
--- phase01/public/salamanders/show.php
+++ phase01/public/salamanders/show.php
@@ -1,20 +1,17 @@
-<?php require_once('../../private/initialize.php'); 
+<?php require_once('../../private/initialize.php'); ?>
 
-// fancy if...else
-
-
+<!-- // fancy if...else
 // if the id is not empty assign it the value from $_GET['id']
 // else $id = 1
-// or use the non-coalesing operator
+// or use the non-coalesing operator -->
+<?php $id = $_GET['id'] ?? '1'; ?>
 
-$pageTitle = 'Salamander Details';
-
-// include the shared path to the header
-
-?>
+<?php $pageTitle = 'Salamander Details'; ?>
+<?php include(SHARED_PATH . '/salamander-header.php'); ?>
 
 <h2>Salamander Details</h2>
-<!-- <p> Page ID: Use the h() function and pass in the id/p> -->
-<p><a href="<?= url_for('/salamanders/index.php'); ?>">&laquo; Back to Salamander List</a></p>
+<p> Page ID: <?php echo h($id); ?> </p>
+<p><a href="<?= urlFor('/salamanders/index.php'); ?>">&laquo; Back to Salamander List</a></p>
 
-<!-- Use the shared path to the salamander footer. -->
+
+<?php include(SHARED_PATH . '/salamander-footer.php'); ?>
